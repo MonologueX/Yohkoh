bae24b0243ea3f9a02d9e3c6ac20d5150ba413ef
MonologueX <3280795699@qq.com>
2019-03-20 23:26:27 +0800

    在线OJ

---
+++ 在线OJ/compiler.cpp
@@ -0,0 +1,20 @@
+#include "httplib.h"
+
+int main()
+{
+    // 命名空间写在里面
+    // 不要写在头文件里
+    using namespace httplib;
+    Server server;
+    // 注册了一个回调函数，这个函数调用时机
+    // 处理 Get 方法的时候
+    // lambda 表达式
+    server.Get("/", [](const Request& req, Response& resp)
+            {
+                (void)req;
+                // 根据具体的场景，根据请求，计算出响应结果
+                resp.set_content("<html>汤老湿，你好啊</html>", "test/html");
+            });
+    server.listen("0.0.0.0", 9001);
+    return 0;
+}
