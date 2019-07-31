8cd79bcb83039ee55b1bfc0ffbc27c23278f73a7
MonologueX <3280795699@qq.com>
2019-07-31 17:58:16 +0800

    1的数目

---
+++ 算法/1的数目/Sumls.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+	int Sumls(int num)
+	{
+		m_factor = 1;
+		m_count = 0;
+		while (num / m_factor)
+		{
+			m_higher = num / (m_factor * 10);
+			m_lower = num % m_factor;
+			m_cur = (num / m_factor) % 10;
+			switch (m_cur)
+			{
+			case 0:
+				m_count += m_higher*m_factor;
+				break;
+			case 1:
+				m_count += m_higher*m_factor + m_lower + 1;
+				break;
+			default:
+				m_count += m_higher*m_factor + m_factor;
+				break;
+			}
+			m_factor *= 10;
+		}
+		return m_count;
+	}
+private:
+	int m_cur;
+	int m_lower;
+	int m_higher;
+	int m_count;
+	int m_factor;
+};
+
+int main()
+{
+	int num = 0;
+	while (cin >> num)
+	{
+		Solution s;
+		cout << s.Sumls(num) << endl;
+	}
+	return 0;
+}
